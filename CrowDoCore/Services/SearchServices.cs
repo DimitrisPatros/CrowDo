c186eff1c6bf0bce2b841a494316b5ab0d9f5ef0
Alex <katerini.alex@gmail.com>
2019-05-28 16:29:51 +0300

    searchServices

--- CrowDoCore/Services/SearchServices.cs
+++ CrowDoCore/Services/SearchServices.cs
@@ -1,34 +1,203 @@
 ﻿using CrowDoCore.Interfaces;
 using CrowDoCore.Models;
+using CrowDoServices;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CrowDoCore.Services
 {
     public class SearchServices : ISearchServices
     {
-        public Result<List<Project>> AlmostExpireProjects()
+        public Result(Project) Result = new Result(Project);
+
+        public Result<Project> SearchPoject(string title)
         {
-            throw new NotImplementedException();
+            var context = new CrowDoDbContext();
+           
+                
+                var project = context.Set<Project>()
+                   .SingleOrDefault(p => p.ProjectTitle == title);
+
+                var projectsList = new Result<Project>()
+                {
+                    Data = project
+                };
+
+                if (project == null)
+                {
+                    projectsList.ErrorCode = 0;
+                    projectsList.ErrorText = "Project not found";
+
+                    return projectsList;
+                }
+
+                if (context.SaveChanges() < 1)
+
+                {
+                    projectsList.ErrorCode = 0;
+                    projectsList.ErrorText = "";
+
+                    return projectsList;
+                }
+                return projectsList;
+            
         }
 
         public Result<List<Project>> AvailableProjects()
         {
-            throw new NotImplementedException();
+            var context = new CrowDoDbContext();
+            {
+                var availableProjectsList = new Result<List<Project>>();
+                var availablelist = context.Set<Project>()
+                    .Any(ap => ap.ProjectStatus == true);
+
+                if (availablelist == false)
+                {
+                    availableProjectsList.ErrorCode = 0;
+                    availableProjectsList.ErrorText = "No available projects";
+
+                    return availableProjectsList;
+                }
+
+                return availableProjectsList;
+            }
         }
 
         public Result<List<Project>> FundedProjects()
         {
+            var context = new CrowDoDbContext();
+            {
+                var fundedProjectList = new Result<List<Project>>();
+                var fundedlist = context.Set<Project>()
+                    .Any(fd => fd.ProjectSuccess == true);
+
+                if (fundedlist == false)
+                {
+                    fundedProjectList.ErrorCode = 0;
+                    fundedProjectList.ErrorText = "No funded projects";
+
+                    return fundedProjectList;
+                }
+
+                return fundedProjectList;
+            }
+        }
+
+        public Result<List<Project>> RecentProjects()
+        {
+            var context = new CrowDoDbContext();
+            {
+                var recentListOfProjects = new Result<List<Project>>();
+                var recentlist = context.Set<Project>()
+                    .Where(rp => rp.CreationDate.AddDays(30) >= DateTime.Today)
+                    .Where(rp => rp.ProjectStatus == true);
+
+                if (recentlist == null)
+                {
+                    recentListOfProjects.ErrorCode = 0;
+                    recentListOfProjects.ErrorText = "Recent list not found";
+
+                    return recentListOfProjects;
+                }
+
+                return recentListOfProjects;
+
+
+            }
+        }
+
+        public Result<List<Project>> MostVisitedProjects()
+        {
+            var context = new CrowDoDbContext();
+            //{
+            //    var mostVisitedProjectList = new Result<List<Project>>();
+            //    var mostvisitedlist = context.Set<Project>()
+            //        .OrderByDescending(mv => mv.Visits)
+            //        .Take(10)
+            //        .ToList();
+            //}
             throw new NotImplementedException();
         }
 
-        public Result<List<Project>> LastMonthProjects()
+        public Result<List<User>> TopProjectCreators(int number)
         {
+            var context = new CrowDoDbContext();
+            {
+                //var topProjectCreators = new Result<List<User>>();
+                //var topcreatorlist = context.Set<User>()
+                //    .OrderByDescending(u => u.)
+                //    .Take(20)
+                //    .ToList();
+            }
             throw new NotImplementedException();
         }
 
         public Result<List<Project>> LastWeekProjects()
+        {
+            var context = new CrowDoDbContext();
+            {
+                var lastWeekProjects = new Result<List<Project>>();
+                var lastweekproject = context.Set<Project>()
+                    .Where(w => w.CreationDate.AddDays(7) >= DateTime.Today);
+
+                if (lastweekproject == null)
+                {
+                    lastWeekProjects.ErrorCode = 0;
+                    lastWeekProjects.ErrorText = "Last week project not found";
+
+                    return lastWeekProjects;
+
+                }
+
+                return lastWeekProjects;
+            }
+        }
+
+        public Result<List<Project>> LastMonthProjects()
+        {
+            var context = new CrowDoDbContext();
+            {
+                var lastMonthProject = new Result<List<Project>>();
+                var lastmonthproject = context.Set<Project>()
+                    .Where(m => m.CreationDate.AddDays(30) >= DateTime.Today);
+
+                if (lastmonthproject == null)
+                {
+                    lastMonthProject.ErrorCode = 0;
+                    lastMonthProject.ErrorText = "Last month project not found";
+
+                    return lastMonthProject;
+                }
+
+                return lastMonthProject;
+            }
+        }
+
+        public Result<List<Project>> ProjectByCategory(string categoryName)
+        {
+            var context = new CrowDoDbContext();
+            {
+                var projectByCategory = new Result<List<Project>>();
+                var projectbycategory = context.Set<ProjectCategories>()
+                    .Where(pc => pc.Category.CategoryName == categoryName)
+                    .ToList();
+
+                if (projectbycategory == null)
+                {
+                    projectByCategory.ErrorCode = 0;
+                    projectByCategory.ErrorText = "Not found";
+
+                    return projectByCategory;
+                }
+
+                return projectByCategory;
+            }
+        }
+
+        public Result<List<Project>> ProjectByCreator(string creatorName)
         {
             throw new NotImplementedException();
         }
@@ -38,34 +207,10 @@ namespace CrowDoCore.Services
             throw new NotImplementedException();
         }
 
-        public Result<List<Project>> MostVisitedProjects()
+        public Result<List<Project>> AlmostExpireProjects()
         {
             throw new NotImplementedException();
         }
 
-        public Result<List<Project>> ProjectByCategory(string categoryName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Result<List<Project>> ProjectByCreator(string creatorName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Result<List<Project>> RecentProjects()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Result<Project> SearchPoject(string title)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Result<List<User>> TopProjectCreators(int number)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
--- CrowDoServices/Services/SearchServices.cs
+++ CrowDoServices/Services/SearchServices.cs
@@ -2,6 +2,7 @@
 using CrowDoServices.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CrowDoServices.Services
@@ -9,32 +10,190 @@ namespace CrowDoServices.Services
 
     public class SearchServices : ISearchServices
     {
-        public CrowDoDbContext context;
-        public SearchServices(CrowDoDbContext contexts)
+        public Result<Project> SearchPoject(string title)
         {
-            context = contexts;
-        }
-        public Result<List<Project>> AlmostExpireProjects()
-        {
-            throw new NotImplementedException();
+            var context = new CrowDoDbContext();
+
+            var result = new Result<Project>();
+            var project = context.Set<Project>()
+                .SingleOrDefault(p => p.ProjectTitle == title);
+
+            project.ProjectViews++;
+
+            if (project == null)
+            {
+                result.ErrorCode = 0;
+                result.ErrorText = "Project not found";
+
+                return result;
+            }
+
+            if (context.SaveChanges() < 1)
+
+            {
+                result.ErrorCode = 0;
+                result.ErrorText = "";
+
+                return result;
+            }
+
+            return result;
         }
 
         public Result<List<Project>> AvailableProjects()
         {
-            throw new NotImplementedException();
+            var context = new CrowDoDbContext();
+
+            var result = new Result<List<Project>>();
+            var availablelist = context.Set<Project>()
+                .Any(ap => ap.ProjectStatus == true);
+
+            if (availablelist == false)
+            {
+                result.ErrorCode = 0;
+                result.ErrorText = "No available projects";
+
+                return result;
+            }
+
+            return result;
         }
 
         public Result<List<Project>> FundedProjects()
         {
-            throw new NotImplementedException();
+            var context = new CrowDoDbContext();
+
+            var result = new Result<List<Project>>();
+            var fundedlist = context.Set<Project>()
+                .Any(fd => fd.ProjectSuccess == true);
+
+            if (fundedlist == false)
+            {
+                result.ErrorCode = 0;
+                result.ErrorText = "No funded projects";
+
+                return result;
+            }
+
+            return result;
         }
 
-        public Result<List<Project>> LastMonthProjects()
+        public Result<List<Project>> RecentProjects()
         {
+            var context = new CrowDoDbContext();
+
+            var result = new Result<List<Project>>();
+            var recentlist = context.Set<Project>()
+                .Where(rp => rp.CreationDate.AddDays(30) >= DateTime.Today)
+                .Where(rp => rp.ProjectStatus == true);
+
+            if (recentlist == null)
+            {
+                result.ErrorCode = 0;
+                result.ErrorText = "Recent list not found";
+
+                return result;
+            }
+
+            return result;
+
+        }
+
+        public Result<List<Project>> MostVisitedProjects()
+        {
+            var context = new CrowDoDbContext();
+            {
+                var result = new Result<List<Project>>();
+                var mostvisitedlist = context.Set<Project>()
+                    .OrderByDescending(mv => mv.ProjectViews)
+                    .Take(10)
+                    .ToList();
+
+                if (context.SaveChanges() < 1)
+
+                {
+                    result.ErrorCode = 0;
+                    result.ErrorText = "";
+
+                    return result;
+                }
+
+                return result;
+            }            
+        }
+
+        public Result<List<User>> TopProjectCreators(int number)
+        {
+            var context = new CrowDoDbContext();
+
+            //var result = new Result<List<User>>();
+            //var topcreatorlist = context.Set<User>()
+            //    .OrderByDescending(u => u.)
+            //    .Take(20)
+            //    .ToList();
+
             throw new NotImplementedException();
         }
 
         public Result<List<Project>> LastWeekProjects()
+        {
+            var context = new CrowDoDbContext();
+
+            var result = new Result<List<Project>>();
+            var lastweekproject = context.Set<Project>()
+                .Where(w => w.CreationDate.AddDays(7) >= DateTime.Today);
+
+            if (lastweekproject == null)
+            {
+                result.ErrorCode = 0;
+                result.ErrorText = "Last week project not found";
+
+                return result;
+            }
+
+            return result;
+        }
+
+        public Result<List<Project>> LastMonthProjects()
+        {
+            var context = new CrowDoDbContext();
+
+            var result = new Result<List<Project>>();
+            var lastmonthproject = context.Set<Project>()
+                .Where(m => m.CreationDate.AddDays(30) >= DateTime.Today);
+
+            if (lastmonthproject == null)
+            {
+                result.ErrorCode = 0;
+                result.ErrorText = "Last month project not found";
+
+                return result;
+            }
+
+            return result;
+        }
+
+        public Result<List<Project>> ProjectByCategory(string categoryName)
+        {
+            var context = new CrowDoDbContext();
+
+            var result = new Result<List<Project>>();
+            var projectbycategory = context.Set<ProjectCategories>()
+                .Where(pc => pc.Category.CategoryName == categoryName)
+                .ToList();
+
+            if (projectbycategory == null)
+            {
+                result.ErrorCode = 0;
+                result.ErrorText = "Not found";
+
+                return result;
+            }
+
+            return result;
+        }
+
+        public Result<List<Project>> ProjectByCreator(string creatorName)
         {
             throw new NotImplementedException();
         }
@@ -44,34 +203,10 @@ namespace CrowDoServices.Services
             throw new NotImplementedException();
         }
 
-        public Result<List<Project>> MostVisitedProjects()
+        public Result<List<Project>> AlmostExpireProjects()
         {
             throw new NotImplementedException();
         }
 
-        public Result<List<Project>> ProjectByCategory(string categoryName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Result<List<Project>> ProjectByCreator(string creatorName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Result<List<Project>> RecentProjects()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Result<Project> SearchPoject(string title)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Result<List<User>> TopProjectCreators(int number)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
